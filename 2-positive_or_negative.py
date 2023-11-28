8d21a3d4be2deffe23ebaa69a86c4d7c75e444e0
Obakeng Play <obakengmplay@gmail.com>
2023-11-28 21:22:37 +0200

    python-hello_world

---
+++ 2-positive_or_negative.py
@@ -0,0 +1,8 @@
+import random 
+number = random.randint(-2344, 1000) 
+if number > 0: 
+    print(f"{number} is positive") 
+elif number == 0: 
+    print(f"{number} is zero") 
+else: 
+    print(f"{number} is negative")
---
+++ 6-print_comb3.py
@@ -0,0 +1,6 @@
+for i in range(10): 
+    for j in range(i + 1, 10): 
+        print(f"{i}{j}", end=", " 
+              if i != 5 
+              or j != 9 
+              else "\n")
