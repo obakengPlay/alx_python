004259504efc4a91509e3d2f981a8ca9cdc0e668
Obakeng Play <obakengmplay@gmail.com>
2023-11-30 23:06:16 +0200

    python-functions

--- 6-password.py
+++ 6-password.py
@@ -1,20 +1,17 @@
-def validate_password(password) :
+def validate_password(password):
     if len(password) < 8:
-      return False
+        return False
 
-has_upper = any(char.isupper()
-                 for char in password)
-has_lower = any(char.islower()
-                 for char in password)
-has_digit = any(char.isdigit()
-                 for char in password)
-has_space = ' ' in password
-  
-return has_upper and has_lower and has_digit
- 
- 
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_space = ' ' in password
+
+    return has_upper and has_lower and has_digit and not has_space
+
 print(validate_password("Password123"))
 print(validate_password("abc123"))
 print(validate_password("Password 123"))
 print(validate_password("password123"))
+
  
