0a2ee7af87b12929f2125a512878d37854b5f7f4
obakengPlay <obakengmplay@gmail.com>
2023-12-08 01:02:47 +0200

    python-import_modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # Subtract 1 to exclude the script name itself
 
     # Print the number of arguments
-    print("{} argument{}:".format(num_args, 's' if num_args != 1 else '') + ('.' if num_args == 0 else ''))
+    print("{} argument{}".format(num_args, 's' if num_args != 1 else '') + ('.' if num_args == 0 else ''))
 
     # Print each argument and its position
     for i in range(1, num_args + 1):
