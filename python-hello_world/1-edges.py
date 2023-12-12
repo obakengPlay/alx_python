4d29b1ca830292861eb19ae3ed3b9f34a2932bbe
obakengPlay <obakengmplay@gmail.com>
2023-12-12 23:44:43 +0200

    python-hello_world

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,13 @@
-word = "programming" 
+word = "Holberton" 
+word_first_3 = word[:3]
+word_last_2 = word[-2:] 
+middle_word = word[1:-1] 
+print(word_first_3) 
+print(word_last_2) 
+print(middle_word)
+print()
+
+word = "school" 
 word_first_3 = word[:3]
 word_last_2 = word[-2:] 
 middle_word = word[1:-1] 
