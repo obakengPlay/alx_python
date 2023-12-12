87cccd96d60d2eafe204b5656a51c907489fb390
obakengPlay <obakengmplay@gmail.com>
2023-12-12 23:28:54 +0200

    python-hello_world

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,1 +1,3 @@
-print ('Programming is like building a multilingual puzzle')
+orignal_string = ("Programming is like building a multilingual puzzle")
+fixed_string = orignal_string + "  "
+print(fixed_string)
