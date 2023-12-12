f01374feaa18dda681b43a708e34eafe93b291cd
obakengPlay <obakengmplay@gmail.com>
2023-12-13 00:37:12 +0200

    python-hello_world

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,7 @@
 for i in range(10): 
     for j in range(i + 1, 10): 
+        
         print(f"{i}{j}", end=", " 
-              if i != 5 
+              if i != 8 
               or j != 9 
               else "\n".format(i, i))
