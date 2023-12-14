ca207aa6f6ded00d0a4d0efe2c41fe8fa0d85c24
obakengPlay <obakengmplay@gmail.com>
2023-12-14 21:24:26 +0200

    0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,3 +3,52 @@ def is_same_class(obj, a_class):
 
 a=1 
 print(is_same_class(a, int))
+
+''' main_1.py '''
+
+is_same_class = __import__('0-is_same_class').is_same_class
+
+a = True
+print(is_same_class(a, int))
+
+'''main_2.py'''
+
+is_same_class = __import__('0-is_same_class').is_same_class
+
+a = 3.14
+print(is_same_class(a, int))
+
+'''main_3.py'''
+
+is_same_class = __import__('0-is_same_class').is_same_class
+
+a = True
+print(is_same_class(a, object))
+
+'''main_4.py'''
+
+is_same_class = __import__('0-is_same_class').is_same_class
+
+a = None
+print(is_same_class(a, object))
+
+'''main_5.py'''
+
+is_same_class = __import__('0-is_same_class').is_same_class
+
+a = None
+print(is_same_class(a, list))
+
+'''main_6.py'''
+
+is_same_class = __import__('0-is_same_class').is_same_class
+
+a = [1, 2, 3]
+print(is_same_class(a, list))
+
+'''main_7.py'''
+
+is_same_class = __import__('0-is_same_class').is_same_class
+
+a = [1, 2, 3]
+print(is_same_class(a, object))
