69d7ee56e62a8b03a211f4d4145a404a2c724d7b
obakengPlay <obakengmplay@gmail.com>
2023-12-14 21:45:07 +0200

    0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,8 +2,10 @@ class SameClass:
     @staticmethod
     def is_same_class(obj, a_class):
         return type(obj) is a_class
-    
-is_same_class = SameClass()
+ 
+ #creating an instance of sameclass    
+same_obj = SameClass()
 
 a=1 
-print(is_same_class.is_same_class(a, int))
+#using method through the object instance 
+print(same_obj.is_same_class(a, int))
