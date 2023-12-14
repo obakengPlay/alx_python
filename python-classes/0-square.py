2417b1247324b590f587e6faf3dcb6050e783e4b
obakengPlay <obakengmplay@gmail.com>
2023-12-14 22:09:04 +0200

    python-classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,24 @@
-'''square class'''
-
-class Square: 
-    def __init__(self, __size): 
-        self.__size = __size 
+"""
+    this is a class
     
+"""
+
+class Square:
+    """
+    Squaring the number
+    
+    """
+    def __init__(self, __size): 
+            self.__size = __size
+            
+#create an instance of class
 my_square = Square(3) 
+            
 # Displaying the type of my_square object 
 print(type(my_square)) 
 # Displaying only the __size attribute value 
 print(my_square.__dict__)
 #Char_length = len('text')
 #print("Character length:",Char_length)
+
+
