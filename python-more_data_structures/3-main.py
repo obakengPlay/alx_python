8587bddc0880ac21a56f21c027acceaae1c98272
obakengPlay <obakengmplay@gmail.com>
2024-01-12 01:05:00 +0200

    python-more_data_structures

---
+++ python-more_data_structures/3-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+common_elements = __import__('1-common_elements').common_elements
+
+set_1 = { "Python", "Javascript" }
+set_2 = { "Python", "Javascript" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
