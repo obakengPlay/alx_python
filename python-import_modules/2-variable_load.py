a8331b9c493a1948253a9af80adee11ce0d0b869
obakengPlay <obakengmplay@gmail.com>
2024-01-11 22:47:07 +0200

    python-import_modules

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-a = 98
+from variable_load_2 import a 
 
-# Import the variable 'a' from variable_load_2.py
-from variable_load_2.py import a
 
-# Print the value of the variable 'a'
-print(a)
-
+if __name__ == "__main__":
+    print(a)
