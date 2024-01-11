af0a18851371d8761bd4b22228e4accb98f7016a
obakengPlay <obakengmplay@gmail.com>
2024-01-12 00:14:36 +0200

    python-data_structures

--- 0-no_c.py
+++ 0-no_c.py
@@ -1,9 +1,7 @@
 def no_c(my_string):
-
-    new_string = ''.join(char for char in my_string if char not in {'c', 'C'})
-    return new_string
-
-
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun"))
+    results = ""
+    for char in my_string:
+        if char not in 'cC':
+            results += char
+    return results 
+  
